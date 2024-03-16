4442a6bf413f426dcc9454fafb4f02bf54fb44d7
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-16 23:50:31 +0600

    Update Cards2.jsx

--- src/components/Sections/slider4/Cards2.jsx
+++ src/components/Sections/slider4/Cards2.jsx
@@ -20,7 +20,7 @@ export default function Cards2() {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get("http://localhost:3001/api/products");
+      const response = await axios.get("https://adminpanellive.vercel.app/api/products");
       setProducts(response.data.products.slice(6, 11)); // Slice to get only 6 products
     } catch (error) {
       console.error("Error fetching products:", error);
