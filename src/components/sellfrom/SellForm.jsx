34fe0ac7c84ed328043b1841bf7626ef00b0dfe2
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-17 00:05:54 +0600

    Update SellForm.jsx

--- src/components/sellfrom/SellForm.jsx
+++ src/components/sellfrom/SellForm.jsx
@@ -119,7 +119,7 @@ export default function SellForm() {
 
   const handlePostSubmit = async () => {
     try {
-      const response = await fetch("http://localhost:3001/api/products", {
+      const response = await fetch("https://adminpanellive.vercel.app/api/products", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
