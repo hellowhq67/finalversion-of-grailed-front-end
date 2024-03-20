99daf285d5b61c47b83329cbe109fac794b05ca6
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-20 23:15:16 +0600

    Update Womenswere.jsx

--- src/components/Sections/Womenswere/Womenswere.jsx
+++ src/components/Sections/Womenswere/Womenswere.jsx
@@ -5,27 +5,27 @@ const data = [
   {
     img: "https://media-assets.grailed.com/prd/misc/896ade0245e148a0839fcc14ef64f0e0?w=180&h=180&fit=clip&q=40&auto=format",
     title: " Tops",
-    path: "http://localhost:3000/products/cetagory/womenswear/top",
+    path: "/products/cetagory/womenswear/top",
   },
   {
     img: "https://media-assets.grailed.com/prd/misc/8220856346fc451daf8563553d1fa35d?w=180&h=180&fit=clip&q=40&auto=format",
     title: " Bottoms",
-    path: "http://localhost:3000/products/cetagory/womenswear/bottoms",
+    path: "/products/cetagory/womenswear/bottoms",
   },
   {
     img: "https://media-assets.grailed.com/prd/misc/a43b20a9b07649c18ebb20ebc274629d?w=180&h=180&fit=clip&q=40&auto=format",
     title: " Ootwear",
-    path: "",
+    path: "/products/cetagory/womenswear/top",
   },
   {
     img: "https://media-assets.grailed.com/prd/misc/d7544d6ba08c44a7b93481b9ce04ceae?w=180&h=180&fit=clip&q=40&auto=format",
     title: "Footwear",
-    path: "http://localhost:3000/products/cetagory/womenswear/footwear",
+    path: "/products/cetagory/womenswear/footwear",
   },
   {
     img: "https://media-assets.grailed.com/prd/misc/721a4fb50bd54bb690870aa51c1f266b?w=180&h=180&fit=clip&q=40&auto=format",
     title: " Bags",
-    path: "",
+    path: "/products/cetagory/womenswear/top",
   },
   {
     img: "https://media-assets.grailed.com/prd/misc/06b3f5e0aeb24c94a3a9e01a31ffe209?w=180&h=180&fit=clip&q=40&auto=format",
