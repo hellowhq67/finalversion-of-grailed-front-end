b3b7846958264feb41428ccb0582cf26a79175d4
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-20 15:33:27 +0600

    Update page.js

--- src/app/products/cetagory/womenswear/footwear/page.js
+++ src/app/products/cetagory/womenswear/footwear/page.js
@@ -61,7 +61,7 @@ export default function page() {
   }, []);
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/api/products');
+      const response = await axios.get('https://adminpanellive.vercel.app');
       // Filter products with category "TOPS" and department "MENSWEAR"
       const filteredProducts = response.data.products.filter(product => product.category === "FOOTWEAR" && product.department === "WOMENSWEAR");
       setProducts(filteredProducts);
