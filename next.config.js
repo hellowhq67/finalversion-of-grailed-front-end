cb0e2f8e3e39fcc497a09bfa2a8bc81578efe764
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-27 23:56:35 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://adminpanellive.vercel.app/", // Set your frontend origin
+            value: "https://finalversion-of-grailed-front-end.vercel.app", // Set your frontend origin
           },
           {
             key: "Access-Control-Allow-Methods",
@@ -20,11 +20,62 @@ const nextConfig = {
         ],
       },
       {
-        source: "/api/follow/:path*", // Match any API route
+        source: "/api/products/:path*", // Match any API route
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3001", // Set your frontend origin
+            value: "*", // Set your frontend origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+        {
+        source: "/api/products/total/:path*", // Match any API route
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your frontend origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+              {
+        source: "/api/blog/:path*", // Match any API route
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your frontend origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+      {
+        source: "/api/contact/:path*", // Match any API route
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your frontend origin
           },
           {
             key: "Access-Control-Allow-Methods",
@@ -37,7 +88,7 @@ const nextConfig = {
         ],
       },
     ];
-  }, 
+  }
 };
 
 module.exports = nextConfig;
