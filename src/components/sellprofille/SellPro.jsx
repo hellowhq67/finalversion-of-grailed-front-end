609669ce44d5a327c30a0691fb008011729ba787
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-28 02:22:25 +0600

    Update SellPro.jsx

--- src/components/sellprofille/SellPro.jsx
+++ src/components/sellprofille/SellPro.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 import { Rating, StarIcon } from "@mui/material";
 const fetchProducts = async () => {
   try {
-    const response = await axios.get("https://adminpanellive.vercel.app/api/products/total");
+    const response = await axios.get("https://adminpanellive.vercel.app/api/total");
     return response.data.products;
   } catch (error) {
     console.error("Error fetching products:", error);
