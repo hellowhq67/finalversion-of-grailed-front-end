9cac2c07e7bd61982cddcddd17c2d32e8a3e240d
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-28 03:46:02 +0600

    Update ProductDetail.jsx

--- src/components/product details/ProductDetail.jsx
+++ src/components/product details/ProductDetail.jsx
@@ -446,30 +446,31 @@ function ProductDetail({ productId }) {
                 {product.condition}
               </p>
               <h1 className={styles.price}>${product.floorPrice}</h1>
-              <div style={{ display: "flex", alignItems: "center" }}>
+                          <div style={{display:"flex",alignItems:"center"}}>
+                <span style={{transform:"translateY(-3px)"}}> {"+"}</span>
+                <span> <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-currency-dollar"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
+                  </svg></span>
                 <span>
-                  {"+"} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-  <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>
-</svg>{product.shippings} Shipping —{userData.location} to
+                 
+               
+                  {product.shippings} Shipping —{userData.location} to
+                </span>{" "}
+                <span>
+                <select style={{ border: "none" }} name="" id="">
+                  <option value="">USA </option>
+                  <option value="">UK</option>
+                  <option value="">Europe</option>
+                  <option value="">Other</option>
+                </select>
                 </span>
-                <div style={{ transform: "translateY(-3px)", margin: "4px" }}>
-                  <FormControl variant="standard" sm={{ m: 1 }}>
-                 <InputLabel id="demo-simple-select-standard-label">{selectedShipping}</InputLabel>
-                    <Select
-                      labelId="shipping-label"
-                      id="shipping"
-                      value={selectedShipping}
-                      onChange={handleShippingChange}
-                      label="Shipping"
-                      sx={{ fontSize: "10px" }}
-                    >
-                      <MenuItem value="United States">United States</MenuItem>
-                      <MenuItem value="Asia">Asia</MenuItem>
-                      <MenuItem value="Europe">Europe</MenuItem>
-                      <MenuItem value="Canada">Canada</MenuItem>
-                    </Select>
-                  </FormControl>
-                </div>
               </div>
 
               {!user ? (
