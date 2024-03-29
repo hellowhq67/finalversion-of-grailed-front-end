2e07b21d815dd489c99577485069d7691679fd54
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-30 03:05:39 +0600

    Update Edit.jsx

--- src/components/profile/Edit.jsx
+++ src/components/profile/Edit.jsx
@@ -23,7 +23,9 @@ function Edit() {
     updateProfile(displayName, bio, phoneNumber, location,profileimgae);
     toast.success("Profile updated successfully!");
   };
-
+if(!user){
+  return null;
+}
   return (
     <div>
       <ToastContainer />
