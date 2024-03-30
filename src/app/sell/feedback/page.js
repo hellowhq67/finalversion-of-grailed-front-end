9ab8aff30c6d898db0f7b575f1ccc239c97c414b
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-30 18:11:09 +0600

    Add files via upload

---
+++ src/app/sell/feedback/page.js
@@ -0,0 +1,12 @@
+import Feedbacks from '@/components/sellprofille/feedback/Feebacks'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Feedbacks/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/sell/sold/page.js
@@ -0,0 +1,12 @@
+import Sold from '@/components/sellprofille/sold/Sold'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Sold/>
+    </div>
+  )
+}
+
+export default page
